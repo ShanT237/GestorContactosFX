57e48fd4b268527865e0e6097e539b350709e3e8
ShanT237 <santiago.rodriguezt@uqvirtual.edu.co>
2025-03-26 05:35:18 -0500

    V1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,7 @@ module co.edu.uniquindio.gestorcontactosfx {
 
 
     opens co.edu.uniquindio.gestorcontactosfx to javafx.fxml;
+    opens co.edu.uniquindio.gestorcontactosfx.Controller to javafx.fxml;
     exports co.edu.uniquindio.gestorcontactosfx;
+    exports co.edu.uniquindio.gestorcontactosfx.Controller;
 }
