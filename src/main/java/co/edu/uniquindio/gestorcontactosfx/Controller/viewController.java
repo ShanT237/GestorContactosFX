a8d66bcbd99c668610a2382d36347c50927adb6e
Yesuu45 <yesuue.arost@uqvirtual.edu.co>
2025-03-25 22:41:48 -0500

    controladores, modelo  y vista union

--- src/main/java/co/edu/uniquindio/gestorcontactosfx/Controller/viewController.java
+++ src/main/java/co/edu/uniquindio/gestorcontactosfx/Controller/viewController.java
@@ -1,98 +1,142 @@
 package co.edu.uniquindio.gestorcontactosfx.Controller;
 
+import co.edu.uniquindio.gestorcontactosfx.Model.Usuario;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
-import javax.swing.text.html.ImageView;
-import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.net.URL;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 public class viewController {
 
+    @FXML
+    private ResourceBundle resources;
 
-        @FXML // ResourceBundle that was given to the FXMLLoader
-        private ResourceBundle resources;
+    @FXML
+    private URL location;
 
-        @FXML // URL location of the FXML file that was given to the FXMLLoader
-        private URL location;
+    @FXML
+    private TextField txtNombre;
 
-        @FXML // fx:id="txtNombre"
-        private TextField txtNombre; // Value injected by FXMLLoader
+    @FXML
+    private TableColumn<Usuario, String> colApellido;
 
-        @FXML // fx:id="colApellido"
-        private TableColumn<?, ?> colApellido; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtTelefono;
 
-        @FXML // fx:id="txtTelefono"
-        private TextField txtTelefono; // Value injected by FXMLLoader
+    @FXML
+    private TableColumn<Usuario, LocalDate> colFechaNacimieto;
 
-        @FXML // fx:id="colFechaNacimieto"
-        private TableColumn<?, LocalDate> colFechaNacimieto; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtEmail;
 
-        @FXML // fx:id="txtEmail"
-        private TextField txtEmail; // Value injected by FXMLLoader
+    @FXML
+    private ImageView imgFotoPerfil;
 
-        @FXML // fx:id="imgFotoPerfil"
-        private ImageView imgFotoPerfil; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtApellido;
 
-        @FXML // fx:id="txtApellido"
-        private TextField txtApellido; // Value injected by FXMLLoader
+    @FXML
+    private TableColumn<Usuario, String> colNombre;
 
-        @FXML // fx:id="colNombre"
-        private TableColumn<?, String > colNombre; // Value injected by FXMLLoader
+    @FXML
+    private DatePicker dateNacimiento;
 
-        @FXML // fx:id="dateNacimiento"
-        private DatePicker dateNacimiento; // Value injected by FXMLLoader
+    @FXML
+    private TableView<Usuario> tablaDeContenido;
 
-        @FXML // fx:id="tablaDeContenido"
-        private TableView<?> tablaDeContenido; // Value injected by FXMLLoader
+    @FXML
+    private TableColumn<Usuario, String> colTelefono;
 
-        @FXML // fx:id="colTelefono"
-        private TableColumn<?, String > colTelefono; // Value injected by FXMLLoader
+    @FXML
+    private TableColumn<Usuario, String> colEmail;
 
-        @FXML // fx:id="colEmail"
-        private TableColumn<?, String> colEmail; // Value injected by FXMLLoader
+    private final ObservableList<Usuario> listaUsuarios = FXCollections.observableArrayList();
 
-        @FXML
-        void seleccionarFoto(ActionEvent event) {
+    @FXML
+    void seleccionarFoto(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Imágenes", "*.jpg", "*.jpeg", "*.png"));
+        File file = fileChooser.showOpenDialog(null);
 
-        }
-
-        @FXML
-        void crearEstudiante(ActionEvent event) {
-
-        }
-
-        @FXML
-        void eliminarEstudiante(ActionEvent event) {
-
-        }
-
-        @FXML
-        void actualizarEstudiante(ActionEvent event) {
-
-        }
-
-        @FXML
-            // This method is called by the FXMLLoader when initialization is complete
-        void initialize() {
-            assert txtNombre != null : "fx:id=\"txtNombre\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert colApellido != null : "fx:id=\"colApellido\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert txtTelefono != null : "fx:id=\"txtTelefono\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert colFechaNacimieto != null : "fx:id=\"colFechaNacimieto\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert txtEmail != null : "fx:id=\"txtEmail\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert imgFotoPerfil != null : "fx:id=\"imgFotoPerfil\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert txtApellido != null : "fx:id=\"txtApellido\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert colNombre != null : "fx:id=\"colNombre\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert dateNacimiento != null : "fx:id=\"dateNacimiento\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert tablaDeContenido != null : "fx:id=\"tablaDeContenido\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert colTelefono != null : "fx:id=\"colTelefono\" was not injected: check your FXML file 'inicio.fxml'.";
-            assert colEmail != null : "fx:id=\"colEmail\" was not injected: check your FXML file 'inicio.fxml'.";
-
+        if (file != null) {
+            imgFotoPerfil.setImage(new Image(file.toURI().toString()));
         }
     }
+
+    @FXML
+    void crearEstudiante(ActionEvent event) {
+        String nombre = txtNombre.getText();
+        String apellido = txtApellido.getText();
+        String telefono = txtTelefono.getText();
+        String email = txtEmail.getText();
+        LocalDate fechaNacimiento = dateNacimiento.getValue();
+
+        Usuario nuevoUsuario = new Usuario(null, fechaNacimiento, email, telefono, apellido, nombre);
+        listaUsuarios.add(nuevoUsuario);
+        tablaDeContenido.setItems(listaUsuarios);
+
+        limpiarCampos();
+    }
+
+    @FXML
+    void eliminarEstudiante(ActionEvent event) {
+        Usuario seleccionado = tablaDeContenido.getSelectionModel().getSelectedItem();
+        if (seleccionado != null) {
+            listaUsuarios.remove(seleccionado);
+        }
+    }
+
+    @FXML
+    void actualizarEstudiante(ActionEvent event) {
+        Usuario seleccionado = tablaDeContenido.getSelectionModel().getSelectedItem();
+        if (seleccionado != null) {
+            seleccionado.setNombre(txtNombre.getText());
+            seleccionado.setApellido(txtApellido.getText());
+            seleccionado.setTelefono(txtTelefono.getText());
+            seleccionado.setEmail(txtEmail.getText());
+            seleccionado.setFechaNacimiento(dateNacimiento.getValue());
+            tablaDeContenido.refresh();
+        }
+    }
+
+    @FXML
+    void initialize() {
+        assert txtNombre != null : "fx:id=\"txtNombre\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert colApellido != null : "fx:id=\"colApellido\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert txtTelefono != null : "fx:id=\"txtTelefono\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert colFechaNacimieto != null : "fx:id=\"colFechaNacimieto\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert txtEmail != null : "fx:id=\"txtEmail\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert imgFotoPerfil != null : "fx:id=\"imgFotoPerfil\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert txtApellido != null : "fx:id=\"txtApellido\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert colNombre != null : "fx:id=\"colNombre\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert dateNacimiento != null : "fx:id=\"dateNacimiento\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert tablaDeContenido != null : "fx:id=\"tablaDeContenido\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert colTelefono != null : "fx:id=\"colTelefono\" was not injected: check your FXML file 'inicio.fxml'.";
+        assert colEmail != null : "fx:id=\"colEmail\" was not injected: check your FXML file 'inicio.fxml'.";
+
+        colNombre.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getNombre()));
+        colApellido.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getApellido()));
+        colTelefono.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getTelefono()));
+        colEmail.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getEmail()));
+        colFechaNacimieto.setCellValueFactory(cellData -> new javafx.beans.property.SimpleObjectProperty<>(cellData.getValue().getFechaNacimiento()));
+
+        tablaDeContenido.setItems(listaUsuarios);
+    }
+
+    private void limpiarCampos() {
+        txtNombre.clear();
+        txtApellido.clear();
+        txtTelefono.clear();
+        txtEmail.clear();
+        dateNacimiento.setValue(null);
+    }
+}
